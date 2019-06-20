8d08882b9d1b636ffc0ceaf81699cc5f1c51d446
Nian <nian@nian.net>
2019-06-20 11:36:10 -0700

    Configure browser-sync into webpack (Laravel Mix)

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,10 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
+
+if (!mix.inProduction()) {
+    mix.browserSync({
+        proxy: process.env.HOST + ':' + process.env.PORT,
+        notify: false,
+    });
+}
