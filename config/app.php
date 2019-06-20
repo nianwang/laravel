d4f7a81d5ff7f7e392ade9052d0b26d539db1594
Nian <nian@nian.net>
2019-06-20 10:18:33 -0700

    Configure feature flag, database migration, enable in app as provider.
    Reference: https://github.com/friendsofcat/laravel-feature-flag/

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         /*
          * Package Service Providers...
          */
+        FriendsOfCat\LaravelFeatureFlags\FeatureFlagsProvider::class,
 
         /*
          * Application Service Providers...
